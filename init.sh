90199877c5cfbe79e3d445e393bfc47f8ee77df7
nagodon <nagodon@gmail.com>
2013-06-23 10:02:34 +0900

    add dot files & init files

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+HOME=`pwd`
+ln -s ./files/dircolors $HOME/.dircolors
+ln -s ./files/gitignore $HOME/.gitignore
+ln -s ./files/inputrc $HOME/.inputrc
+ln -s ./files/screenrc $HOME/.screenrc
+ln -s ./files/vimrc $HOME/.vimrc
