9a440bb1af2bcda3adacf54449226ef767424257
nagodon <nagodon@gmail.com>
2014-02-05 12:11:41 +0900

    gitconfigの更新

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 WORK_DIR=`dirname $0`
 
 ln -sfn $WORK_DIR/files/dircolors $HOME/.dircolors
+ln -sfn $WORK_DIR/files/gitconfig $HOME/.gitconfig
 ln -sfn $WORK_DIR/files/gitignore $HOME/.gitignore
 ln -sfn $WORK_DIR/files/inputrc $HOME/.inputrc
 ln -sfn $WORK_DIR/files/screenrc $HOME/.screenrc
