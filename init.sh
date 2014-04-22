7892e9bf231d8903196d40c8d1b569aba88a4f6e
nagodon <nagodon@gmail.com>
2014-04-22 15:52:58 +0900

    バックアップ関連の設定に変更

--- init.sh
+++ init.sh
@@ -8,3 +8,7 @@ ln -sfn $WORK_DIR/files/gitignore $HOME/.gitignore
 ln -sfn $WORK_DIR/files/inputrc $HOME/.inputrc
 ln -sfn $WORK_DIR/files/screenrc $HOME/.screenrc
 ln -sfn $WORK_DIR/files/vimrc $HOME/.vimrc
+
+if [ ! -d x"$HOME/.vim" -o ! -d x"$HOME/.vim/tmp" ]; then
+	mkdir -p $HOME/.vim/tmp/{swap,backup}
+fi
