b89fad9e9680754ab2ee6f4a304a3624bee13203
nagodon <nagodon@gmail.com>
2014-11-18 11:39:58 +0900

    editrc追加

--- init.sh
+++ init.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-WORK_DIR=`dirname $0`
+WORK_DIR=$(cd $(dirname $0);pwd)
 
 ln -sfn $WORK_DIR/files/dircolors $HOME/.dircolors
 ln -sfn $WORK_DIR/files/gitconfig $HOME/.gitconfig
 ln -sfn $WORK_DIR/files/gitignore $HOME/.gitignore
 ln -sfn $WORK_DIR/files/inputrc $HOME/.inputrc
+ln -sfn $WORK_DIR/files/editrc $HOME/.editrc
 ln -sfn $WORK_DIR/files/screenrc $HOME/.screenrc
 ln -sfn $WORK_DIR/files/vimrc $HOME/.vimrc
 
